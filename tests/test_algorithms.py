1c09d03e5154dbbe66e2387dc28d7b69292dd536
Jatin Singh <jatinsingh6654@gmail.com>
2026-08-06 18:23:05 +0530

    code to test base line algos

---
+++ tests/test_algorithms.py
@@ -0,0 +1,19 @@
+import pytest
+from neuroroute.network.topology import TopologyManager
+from neuroroute.network.algorithms import Dijkstras
+
+@pytest.fixture
+def sample_topo():
+    topo = TopologyManager()
+    topo.load_topology("configs/test-topology.json")
+    # topo.print_graph()        # captured stdout will also be displayed
+    return topo
+
+def test_dijkstras(sample_topo):
+    router = Dijkstras(sample_topo)
+
+    path = router.get_shortest_path("nodeA", "nodeD")
+    assert path == ["nodeA", "nodeB", "nodeD"]
+
+    next_hop = router.get_next_hop("nodeA", "nodeD")
+    assert next_hop == "nodeB"
